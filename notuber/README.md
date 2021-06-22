3862adccf6a2d72fd3bb8ae7c9bc7c49005bd2e9
Leon Gao <im.leongao@gmail.com>
2021-06-22 22:43:29 +0800

    create a page that displays a Google Map of vehicles with dynamic coordinates

--- notuber/README.md
+++ notuber/README.md
@@ -1,28 +1,12 @@
-### This lab creates a page that displays a Google Map of 6 vehicles with latitude and longitude. 
+### This lab creates a page that displays a Google Map of vehicles with latitude and longitude from the ride request API: https://jordan-marsh.herokuapp.com/rides. 
 
-* The map takes up the entire page.
-* A CSS file is required in order for the Google Map to even work.
-* Use a separate file for the JavaScript.
-* For now, the map shall be centered on latitude = 42.352271, longitude = -71.05524200000001. This location is South Station in Boston, MA.
+* The map shows your geolocation with the default red pin Google uses.
+* Make a request to the ride-hailing API, providing your username, latitude and longitude.
 * Each vehicle on the map shall be a marker with ths icon: ![car.png](car.png "car.png")
+* Upon clicking on your marker on the map, display an information window (a.k.a., infowindow) noting the closest vehicle from where you are including the distance away in miles.
+* Render a polyline (any color) that connects "your" marker to the marker of the closest vehicle.
+* Upon clicking on a marker for a vehicle, show popup infowindow noting how far away the vehicle is, in miles, from you.
 
-### Performance Comparison Table
+### The piazza is really helpful for me to solve most of the problems.
 
-| | requests  | transferred | Total resources  | Finish time | DOMContentLoaded time | Load time |
-| ------------- |:-------------:| ------------- | ------------- |------------- |------------- |:-------------:|
-| Before optimization | 67 | 10.8kB | 1.2MB | 1.45s | 25ms | 45ms |
-| After optimization | 66 | 10.1kB  | 1.2MB | 1.25s | 20ms | 37ms |
-
-### Performance Comparison Analysis
-
-* The values in the above table may vary a little, but the overall result is that our optimizaiton steps work very well.  
-
-* Specifically, the requests reduce by one because we put the javascript codes into the html. Thus we do not need to request the js file any more.  
-
-* Besides, the transferred file also reduces because we minify the css and js codes.  
-
-* Therefore, the finish time, DOMContentLoaded time and load time all decrease in some degree.
-
-The piazza is really helpful for me to solve most of the problems.
-
-I spent approximately 4 hours to complete this lab.
+### I spent approximately 8 hours to complete this lab.
--- notuber/gmap.js
+++ notuber/gmap.js
@@ -1,51 +1,127 @@
 let map;
 
 function initMap() {
+    // Define a global info window
+    var infowindow = new google.maps.InfoWindow();
+
     // Create a map
-    var map = new google.maps.Map(document.getElementById("gmap"), {
-        center: {lat: 42.352271, lng: -71.05524200000001},
-        zoom: 14
+    map = new google.maps.Map(document.getElementById("gmap"), {
+        zoom: 1
     });
 
     // Create center marker
     var center_marker = new google.maps.Marker({
-        position: {lat: 42.352271, lng: -71.05524200000001},
-        title: "South Station, Boston, MA",
+        title: "my position",
         map: map
     });
 
-    // Define a global info window
-    var infowindow = new google.maps.InfoWindow();
+    // Get HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    }
+
+    // Set geolocation as the center and add marker
+    function showPosition(position) {
+        pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+        };
+        map.setCenter(pos);
+        center_marker.setPosition(pos);
+        getVehicleInfoList(pos);
+    }
+
+    // Handle geolocation errors
+    function showError(error) {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+              alert("User denied the request for Geolocation.");
+              break;
+            case error.POSITION_UNAVAILABLE:
+              alert("Location information is unavailable.");
+              break;
+            case error.TIMEOUT:
+              alert("The request to get user location timed out.");
+              break;
+            case error.UNKNOWN_ERROR:
+              alert("An unknown error occurred.");
+              break;
+          }
+    }
+    
+    function getVehicleInfoList(pos) {
+        var xhttp = new XMLHttpRequest();
+        var url = "https://jordan-marsh.herokuapp.com/rides";
+        var params = "username=pPHjoZLM&lat=" + pos.lat + "&lng=" + pos.lng;
+        // console.log('params: %s', params);
+        xhttp.open("POST", url, true);
+    
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                calMinDistance(pos, JSON.parse(xhttp.responseText));
+            }
+        };
+        xhttp.send(params);
+    }
+
+    function calMinDistance(myPos, jsonData) {
+        const dist_arr = [];
+        var my_pos = new google.maps.LatLng(myPos.lat, myPos.lng);
+        for (let i = 0; i < jsonData.length; i++) {
+            var car_pos = new google.maps.LatLng(jsonData[i].lat, jsonData[i].lng);
+            var distance = google.maps.geometry.spherical.computeDistanceBetween(my_pos, car_pos);
+            dist_arr[i] = distance; 
+        };
+        var min_dist = Math.min.apply(Math, dist_arr);
+        var min_index = dist_arr.indexOf(min_dist);
+        var min_dist_car = jsonData[min_index].username;
+
+        addInfoWindowListener(min_dist, min_dist_car);
+        addPolyline(myPos, jsonData[min_index].lat, jsonData[min_index].lng);
+        createVehicleMarkers(jsonData, dist_arr);
+    }
 
     // Open info window on click of marker
-    center_marker.addListener("click", () => {
-        infowindow.setContent(center_marker.title);
-        infowindow.open(map, center_marker);
-    });
-
-    // Define positions and IDs for car markers
-    const features = [
-        { position: new google.maps.LatLng(42.3453, -71.0464), id: "mXfkjrFw" },
-        { position: new google.maps.LatLng(42.3662, -71.0621), id: "nZXB8ZHz" },
-        { position: new google.maps.LatLng(42.3603, -71.0547), id: "Tkwu74WC" },
-        { position: new google.maps.LatLng(42.3472, -71.0802), id: "5KWpnAJN" },
-        { position: new google.maps.LatLng(42.3663, -71.0544), id: "uf5ZrXYw" },
-        { position: new google.maps.LatLng(42.3542, -71.0704), id: "VMerzMH8" },
-    ];
-
-    // Create car markers.
-    for (let i = 0; i < features.length; i++) {
-        const car_marker = new google.maps.Marker({
-            position: features[i].position,
-            title: features[i].id,
-            icon: "car.png",
-            map: map
+    function addInfoWindowListener(min_dist, min_dist_car) {
+        center_marker.addListener("click", () => {
+            infowindow.setContent("<p>" + "Closest car: " + min_dist_car + "<br />" + "Distance: " + min_dist * 0.000621371192 + "miles" + "</p>");
+            infowindow.open(map, center_marker);
         });
-        // Open info window on click of marker
-        car_marker.addListener("click", () => {
-            infowindow.setContent(car_marker.title);
-            infowindow.open(map, car_marker);
+    }
+
+    // Render a polyline between my position and the closest car
+    function addPolyline(myPos, min_dist_car_lat, min_dist_car_lng) {
+        const coordsPath = [
+            {lat: myPos.lat, lng: myPos.lng},
+            {lat: min_dist_car_lat, lng: min_dist_car_lng},
+        ];
+        const min_polyline = new google.maps.Polygon({
+            paths: coordsPath,
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
         });
+        min_polyline.setMap(map);
+    }
+
+    function createVehicleMarkers(jsonData, dist_arr) {
+        for (let i = 0; i < jsonData.length; i++) {
+            const car_pos = {lat: jsonData[i].lat, lng: jsonData[i].lng};
+            const car_name = jsonData[i].username;
+            const car_marker = new google.maps.Marker({
+                position: car_pos,
+                title: car_name,
+                icon: "car.png",
+                map: map
+            });
+            // Open info window on click of marker
+            car_marker.addListener("click", () => {
+                infowindow.setContent("<p>" + "Car: " + car_name + "<br />" + "Distance: " + dist_arr[i] * 0.000621371192 + "miles" + "</p>");
+                infowindow.open(map, car_marker);
+            });
+        }
     }
 
 }
